37b18fd22343c9d21af000199a57d585712676ba
Gabriel Paris <otavioparisdev@gmail.com>
2025-04-28 19:48:42 -0300

    Commit Ordenação
    Signed-off-by: Gabriel Paris <otavioparisdev@gmail.com>

---
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,40 @@
+package list.Ordenacao;
+
+import java.util.*;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new Pessoa.ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.54);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.53);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.59);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.52);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
+
